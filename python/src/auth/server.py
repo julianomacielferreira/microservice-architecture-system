dc70a98dcb16ff1a24041df61ef36197adf81f8b
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-07-03 18:57:25 -0300

    Refactor the mysql envs setup.

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -5,18 +5,17 @@ from flask_mysqldb import MySQL
 server = Flask(__name__)
 mysql = MySQL(server)
 
-# config for the application
-server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
-server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
-server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
-server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+mysql_envs = ["MYSQL_HOST", "MYSQL_USER", "MYSQL_PASSWORD", "MYSQL_DB", "MYSQL_PORT"]
+
+for mysql_env in mysql_envs:
+    server.config[mysql_env] = os.environ.get(mysql_env)
+
 
 @server.route("/login", methods=["POST"])
 def login():
     # Getting info from Basic Authentication Header
     auth = request.authorization()
-    if not auth: # request invalid
+    if not auth:  # request invalid
         return "missing credentials", 401
-    
+
     # check db for username and password
