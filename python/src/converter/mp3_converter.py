cadd31ca3890d33dd0fbfe7ab44630631daea3f7
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-07-12 14:26:20 -0300

    Add license comment.

--- python/src/converter/mp3_converter.py
+++ python/src/converter/mp3_converter.py
@@ -21,35 +21,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-
-import pika, json
-
-
-def upload_video_to_mongodb(file, mongoGridFS, rabbitMQChannel, payload):
-    """
-    - Upload the file to mongodb gridfs
-    - Put a message on the queue
-    """
-    try:
-        video_fileID = mongoGridFS.put(file)
-    except Exception as error:
-        return "internal server error", 500
-
-    message = {
-        "video_fileID": str(video_fileID),
-        "mp3_fileID": None,
-        "username": payload["username"],
-    }
-
-    try:
-        rabbitMQChannel.basic_publish(
-            exchange="",
-            routing_key="video",
-            body=json.dumps(message),
-            property=pika.BasicProperties(
-                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
-            ),
-        )
-    except:
-        mongoGridFS.delete(video_fileID)
-        return "internal server error", 500
